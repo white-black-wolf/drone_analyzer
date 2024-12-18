efed75804f927b5f913b1083be39236ed23437bd
wbw <151435718+white-black-wolf@users.noreply.github.com>
2024-12-18 15:34:10 +0300

    Add files via upload

--- search/learning.py
+++ search/learning.py
@@ -4,13 +4,14 @@ from ultralytics import YOLO
 DATA_CONFIG = r'C:\Users\vanoh\OneDrive\Desktop\123\data.yaml'
 
 # Создаём модель (можно использовать предобученную модель)
-model = YOLO('yolov8s.pt')  # Выбираем версию модели: n (nano), s (small), m (medium), l (large)
+model = YOLO('yolov8n.pt')  # Выбираем версию модели: n (nano), s (small), m (medium), l (large)
 
 # Обучаем модель
 model.train(
     data=DATA_CONFIG,       # Путь к файлу data.yaml
     epochs=15,              # Количество эпох обучения
-   
+    
     batch=8,                # Размер батча
-    name='yolo8_drone'      # Название эксперимента
+    name='yolo8_drone',
+        # Название эксперимента
 )
